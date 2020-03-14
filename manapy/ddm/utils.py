62471c16c3b5703240c3f4b8094e949f4ba82091
kissami <imadki>
2020-03-14 01:53:54 +0100

    test swe

--- manapy/ddm/utils.py
+++ manapy/ddm/utils.py
@@ -849,12 +849,12 @@ def save_paraview_results(w_c, niter, miter, time, dtime, rank, size, cells, nod
     integral_sum = np.zeros(1)
 
 
-#    COMM.Reduce(maxh, integral_sum, MPI.MAX, 0)
-#    if rank == 0:
-#        print(" **************************** Computing ****************************")
-#        print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Saving Results $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-#        print("Iteration = ", niter, "time = ", np.float16(time), "time step = ", np.float16(dtime))
-#        print("max h =", np.float16(integral_sum[0]))
+    COMM.Reduce(maxh, integral_sum, MPI.MAX, 0)
+    if rank == 0:
+        print(" **************************** Computing ****************************")
+        print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Saving Results $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+        print("Iteration = ", niter, "time = ", np.float16(time), "time step = ", np.float16(dtime))
+        print("max h =", np.float16(integral_sum[0]))
 #
 #    meshio.write_points_cells("results/visu"+str(rank)+"-"+str(miter)+".vtu",
 #                              points, elements, cell_data=data, file_format="vtu")
